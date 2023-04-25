e20bda5f3427a5d2474782dda67d1bbd662efac7
Kuleb1n <v_vpyt@mail.ru>
2023-04-25 22:13:47 +0300

    tests.py

--- to_do/tests.py
+++ to_do/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserLoginViewTestCase(TestCase):
+    pass
+
+
+class UserRegistrationViewTestCase(TestCase):
+    pass
+
+
+class UserCreateViewTestCase(TestCase):
+    pass
